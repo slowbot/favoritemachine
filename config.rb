ad4cf97a1af5c046f58c0ccb36ea106ec625865a
slowbot <jesse@slowbot.com>
2015-04-02 00:43:49 -0700

    Changing build directory

--- config.rb
+++ config.rb
@@ -113,12 +113,12 @@ end
   # deploy.flags = "-rltgoDvzO --no-p --del -e" # add custom flags, default: -avze
 # end
 
-activate :deploy do |deploy|
-  deploy.method = :git
-  # Optional Settings
-  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
-  # deploy.branch   = 'custom-branch' # default: gh-pages
-  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
-end
+# activate :deploy do |deploy|
+#   deploy.method = :git
+#   # Optional Settings
+#   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+#   # deploy.branch   = 'custom-branch' # default: gh-pages
+#   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+#   # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+# end
 
